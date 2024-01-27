23e6c031decf8a4debf98af909324a020b470a13
Omar Juvera <omar.myspam@gmail.com>
2024-01-27 03:45:08 -0800

    updates

--- Python/snippets/sandbox.py
+++ Python/snippets/sandbox.py
@@ -5,12 +5,5 @@ Spyder Editor
 This is a temporary script file.
 """
 
-userRange = int(input("Enter a range: "))
-
-print("\n Range list:")
-
-userRange += 1
-objRange = range(1, userRange)
-for rangeList in objRange:
-    print(rangeList, "___")
-
+o = "Omar"
+print( o[0] )
