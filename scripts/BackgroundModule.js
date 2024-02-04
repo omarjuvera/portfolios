9b2640f033cb47039ebd7c05888c7f50e31357ed
Omar Juvera <omar.myspam@gmail.com>
2024-02-04 00:31:38 -0800

    Update BackgroundModule.js

--- scripts/BackgroundModule.js
+++ scripts/BackgroundModule.js
@@ -1,1 +1,20 @@
+<!-- BackgroundModule.js -->
+<script id="background-module">
+    const BackgroundModule = (function () {
+        const backgrounds = [
+            { portfolio: 'Portfolios', url: 'url(https://repository-images.githubusercontent.com/736133296/80d2132b-b291-4b86-a4b6-6b66250785c4)' },
+            { portfolio: 'Cybersecurity', url: 'url(path/to/image1.jpg)' },
+            { portfolio: 'Python', url: 'url(path/to/image2.jpg)' },
+            // Add more portfolios and corresponding URLs as needed
+        ];
 
+        function getBackgroundUrl(portfolioUrl) {
+            const background = backgrounds.find(bg => bg.portfolio === portfolioUrl);
+            return background ? background.url : backgrounds[0].url;
+        }
+
+        return {
+            getBackgroundUrl,
+        };
+    })();
+</script>
